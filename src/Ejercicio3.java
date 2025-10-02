b07089ec56e32b625d488c3881d33f734a41a57f
diegod25122 <diego.camacho@epn.edu.ec>
2025-10-02 15:19:35 -0500

    Ejercicio 3

---
+++ src/Ejercicio3.java
@@ -0,0 +1,12 @@
+//Ejercicio 3: Requisitos para el cuartel
+//Objetivo: Determinar si un estudiante cumple (≥18 años y altura ≥ 1.60 m).
+//Entradas: edad (int), altura (double).
+//Salida: “Cumple” o “No cumple”.
+//Reglas: edad y altura deben ser positivas.
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args){
+
+    }
+}
