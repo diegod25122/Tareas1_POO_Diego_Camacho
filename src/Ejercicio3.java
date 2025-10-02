50e56951c1dfe51449922a7013b5120deb04a920
diegod25122 <diego.camacho@epn.edu.ec>
2025-10-02 15:42:09 -0500

    validar edad

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -6,7 +6,25 @@
 import java.util.Scanner;
 
 public class Ejercicio3 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        String nombre = "";
+        int edad = 0;
+        double altura = 0.0;
+        Scanner ingresarEstudiante = new Scanner(System.in);
+
+        System.out.println("Ingrese el nombre del aspirante: ");
+        nombre = ingresarEstudiante.nextLine();
+
+        System.out.println("Ingrese la edad del estudiante: ");
+        //validamos edades negativas
+        while(true){
+            edad= ingresarEstudiante.nextInt();
+            if(edad<=0 || edad > 100){
+                System.out.println("Ingrese una edad valida");
+            }else{
+                break;
+            }
+        }
 
     }
 }
